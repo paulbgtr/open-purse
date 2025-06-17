1d54e6d4ecbe44bb42dd92c0f5e46de2f93de4fb
paulbgtr <pbg.work@pm.me>
2025-06-17 14:34:39 +0300

    feat: simple home page revealing the features

--- app/new/page.tsx
+++ app/new/page.tsx
@@ -1,13 +1,9 @@
+import { CreatePurseForm } from "@/components/create-purse-form";
+
 export default function NewPage() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1 className="text-4xl font-bold text-center mb-4">
-        Create a New Purse
-      </h1>
-      <p className="text-xl text-gray-600 leading-relaxed">
-        Accept donations without BS. Just create a new purse and you're good to
-        go.
-      </p>
+    <div className="min-h-screen py-8">
+      <CreatePurseForm />
     </div>
   );
 }
