27c640796dd6c00579202692a81347580c54eac3
paulbgtr <pbg.work@pm.me>
2025-06-18 12:41:08 +0300

    fix(db/schema.ts): rename column purseId to hash for consistency with other unique columns feat(db/schema.ts): add new columns avatar, label to wallet_addresses table and title, url to links table refactor(db/schema.ts): update type aliases for InsertUser, InsertWalletAddress, InsertLink to reflect changes in schema

--- lib/db/schema.ts
+++ lib/db/schema.ts
@@ -2,27 +2,31 @@ import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const purses = sqliteTable("purses", {
   id: int("id").primaryKey(),
-  purseId: text("purse_id").notNull().unique(),
+  hash: text("purse_id").notNull().unique(),
   username: text("username").notNull().unique(),
+  avatar: text("avatar"),
   bio: text("bio"),
 });
 
 export const walletAddresses = sqliteTable("wallet_addresses", {
   id: int("id").primaryKey(),
-  userId: int("user_id")
+  purseId: int("purse_id")
     .notNull()
     .references(() => purses.id),
-  address: text("address").notNull().unique(),
+  type: text("type").notNull(),
+  address: text("address").notNull(),
+  label: text("label"),
 });
 
 export const links = sqliteTable("links", {
   id: int("id").primaryKey(),
-  userId: int("user_id")
+  purseId: int("purse_id")
     .notNull()
     .references(() => purses.id),
-  link: text("link").notNull().unique(),
+  title: text("title").notNull(),
+  url: text("url").notNull(),
 });
 
-export type User = typeof purses.$inferSelect;
-export type WalletAddress = typeof walletAddresses.$inferSelect;
-export type Link = typeof links.$inferSelect;
+export type InsertUser = typeof purses.$inferInsert;
+export type InsertWalletAddress = typeof walletAddresses.$inferInsert;
+export type InsertLink = typeof links.$inferInsert;
